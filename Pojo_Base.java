cddf276a54fbf396f8874d410396e7e6e30c1679
Srinathsri21 <srinath.cse21@gmail.com>
2022-05-26 21:06:34 +0530

    Version 1 done

---
+++ Pojo_Base.java
@@ -0,0 +1,18 @@
+package com.pojobase.org;
+
+import com.pojo.org.Pojo__Class;
+
+public class Pojo_Base {
+	
+	public static void main(String[] args) {
+		
+		Pojo__Class a = new Pojo__Class();
+		a.setNumber(20);
+		a.setName("Srinath");
+		System.out.println("Number is:"+ a.getNumber());
+		System.out.println("Namei is:"+ a.getName());
+	
+		
+	}
+
+}
